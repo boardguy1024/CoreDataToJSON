522d0121084c8707198cfe7ad6a9d07322760111
paku <boardguy1024@gmail.com>
2024-01-14 21:06:30 +0900

    Initial Commit

---
+++ CoreDataToJSON/CoreDataToJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataToJSONApp.swift
+//  CoreDataToJSON
+//
+//  Created by paku on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataToJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
